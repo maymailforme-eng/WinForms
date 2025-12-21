a363a44701d178ebd8f87781164ee3e6a2c9e28a
Лев <maymailforme@gmail.com>
2025-12-21 20:27:22 +0300

    HomeWork_1
    Домашние задание: При запуске программы в EditControl 'IDC_EDIT_LOGIN' должно отображаться приглашение
     'Введите имя пользователя'. Когда пользователь наводит курсор на это поле, приглашение исчезает,
    и если пользователь ввел свое имя, то при выходе из текстового поля в нем сохраняется имя пользователя.
     Но если на момент выхода из текстового поля оно остается пустым, то приглашение возвращается.

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -17,22 +17,105 @@ INT WINAPI WinMain(HINSTANCE hInstance, // Дескриптор (handle) текущего экземпля
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg)
-	{
-	case WM_INITDIALOG: break; //Инициализация окна дифлога. Секция отрабатывает один раз, при запуске окна.
-	case WM_COMMAND: //В этой секции обрабатываються нажатия клавишь и другия события.
-		switch (LOWORD(wParam))
-		{
-		case IDOK: MessageBox(hwnd, "Была нажата кнопка", "info", MB_OK | MB_ICONINFORMATION); break;
-		case IDCANCEL: EndDialog(hwnd, 0); break;
 
+
+
+	switch (uMsg)//сравниваем сообщение-ответ полученное от окна hwnd (в данном сучае основное диалоговое)
+	{
+		//инициализация окна диалога .....................................................................................
+		case WM_INITDIALOG: //Инициализация окна дифлога. Секция отрабатывает один раз, при запуске окна.
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN); //код HWND окна ввода логина
+			HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1)); //загружаем иконку
+			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon); //устанавливаем иконку
+			SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)TEXT("Введите имя пользователя")); //дефолтная запись в Edit control
 		}
-		
-		break; 
-	case WM_CLOSE: 
+		break;
+
+
+
+		//обработка нажатия кнопок и т.д............................................................................
+		case WM_COMMAND: //В этой секции обрабатываються нажатия клавишь и другия события.
+		{
+			CONST INT SIZE = 256; //размер для чаровских массивов
+			const char textDef[25] = { "Введите имя пользователя" };
+
+			UINT id = LOWORD(wParam); //колбэк запишет id отправляющего сообщение окна в LOWORD(wParam) (может повтараться)
+			UINT code = HIWORD(wParam); //колбэк запишет шифр события (что произошло) в HIWORD(wParam)
+			HWND hControl = (HWND)lParam; //в lParam колбэк запишет HWND окна отправившего сообщения (уникальный код) 
+
+			switch (code) //шагаем по событиям 
+			{
+				case EN_SETFOCUS: //кликнули в поле
+				{
+					if (id == IDC_EDIT_LOGIN) //если сообщение отправило поле логина
+					{
+						CHAR currentText[SIZE] = {};
+						HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN); //код HWND окна ввода логина
+
+						SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)currentText); //считываем текст в поле
+
+						if (lstrcmp(currentText, TEXT(textDef)) == 0)
+						{
+							SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)TEXT(""));
+						}
+					}
+				}
+				break;
+
+				case EN_KILLFOCUS: //текст покинул поле ввода
+				{
+					if (id == IDC_EDIT_LOGIN)
+					{
+						CHAR currentText[SIZE] = {};
+						HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN); //код HWND окна ввода логина
+
+						SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)currentText); //считываем текст в поле
+						if (lstrcmp(currentText, TEXT("")) == 0)
+						{
+							SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)TEXT(textDef));
+						}
+					}
+				
+				}break;
+
+				case BN_CLICKED: //нажатие кнопки
+				{
+					if (id == IDC_BUTTON_COPY) //кнопка  COPY
+					{
+						CHAR currentText[SIZE] = {};
+						HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN); //код HWND окна ввода логина
+						HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD); // код HWND окна ввода пароля
+						SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)currentText);
+
+						if ((lstrcmp(currentText, TEXT(textDef)) != 0))
+						{
+							SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)currentText);
+						}
+					}
+					else if (id == IDOK) //кнопка ОК
+					{
+						MessageBox(hwnd, "Была нажата кнопка", "info", MB_OK | MB_ICONINFORMATION); break;
+					}
+					else if (id == IDCANCEL) //кнопка Cancel
+					{
+						EndDialog(hwnd, 0);
+					}
+					
+				}
+				break;
+			}
+		}
+		break;
+
+
+	//логика нажатия на крестик .............................................................................................
+	case WM_CLOSE:
 		EndDialog(hwnd, 0); // позволяет закрыть на крестик окно
 		break; // отрабатывает при закрытии окна.
 	}
 
 	return FALSE;
 }
+
+
