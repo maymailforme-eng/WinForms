1158d8e45c7cae1d00b928c80c9ed78bf4b8cb11
Лев <maymailforme@gmail.com>
2026-01-17 19:53:05 +0300

    ConveirDone
    ConveirDone

--- CalckInClass/main.cpp
+++ CalckInClass/main.cpp
@@ -380,14 +380,16 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		static INT		operation = 0;
 		static BOOL		input = FALSE;	
 		static BOOL		input_operation = FALSE;	
+		static BOOL		executed = FALSE;
 
 		CHAR sz_digit[2] = {};
 		CHAR sz_display[MAX_PATH] = {};
 		HWND hEditDisplay = GetDlgItem(hwnd, IDC_DISPLAY);
 		SendMessage(hEditDisplay, WM_GETTEXT, MAX_PATH, (LPARAM)sz_display);
-		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9) //ввод цифр
 		{
 			input_operation = FALSE;
+			executed = FALSE;
 			if (input == FALSE)ZeroMemory(sz_display, sizeof(sz_display));
 			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			if (sz_display[0] == '0' && sz_display[1] != '.')
@@ -428,6 +430,8 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				(a == DBL_MIN ? a : b) = atof(sz_display);
 				input = false;
 			}
+
+			if(!input_operation && !executed)SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
 			operation = LOWORD(wParam);
 			input_operation = TRUE;
 		}
@@ -446,6 +450,7 @@ LRESULT WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			case IDC_BUTTON_SLASH:	a /= b;		break;
 			}
 			input_operation = FALSE;
+			executed = TRUE;
 			if (a != DBL_MIN)
 			{
 				sprintf(sz_display, "%g", a);
